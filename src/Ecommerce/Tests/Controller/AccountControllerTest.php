ebc161a8ce8f8f6f0e4b366f6dc9b8e1e152aa22
YoshiBalboa <bouyouyouyou@gmail.com>
2015-08-26 12:12:01 +0200

    Tests fonctionnels : editEmail + editPassword

--- src/Ecommerce/Tests/Controller/AccountControllerTest.php
+++ src/Ecommerce/Tests/Controller/AccountControllerTest.php
@@ -3,6 +3,8 @@
 namespace Ecommerce\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class AccountControllerTest extends WebTestCase
 {
@@ -23,15 +25,10 @@ class AccountControllerTest extends WebTestCase
 		$this->login = 'test@email.com';
 		$this->password = 'te$tpassword';
 
-		$this->client = static::createClient(array(), array(
-				'PHP_AUTH_USER'	 => $this->login,
-				'PHP_AUTH_PW'	 => $this->password,
-		));
+		$this->client = static::createClient(array(), array());
 
 		$this->container = $this->client->getContainer();
 		$this->em = $this->container->get('doctrine.orm.entity_manager');
-
-		$this->client->followRedirects();
 	}
 
 	public function testCreateAccount()
@@ -84,6 +81,8 @@ class AccountControllerTest extends WebTestCase
 	{
 		fwrite(STDOUT, PHP_EOL . __METHOD__ . PHP_EOL);
 
+		$this->logIn($this->login, $this->password);
+
 		$crawler = $this->client->request('GET', '/account');
 		$this->assertTrue($this->client->getResponse()->isSuccessful());
 		$this->assertEquals('Ecommerce\Controller\AccountController::indexAction', $this->client->getRequest()->attributes->get('_controller'));
@@ -94,6 +93,9 @@ class AccountControllerTest extends WebTestCase
 	{
 		fwrite(STDOUT, PHP_EOL . __METHOD__ . PHP_EOL);
 
+		$this->client->followRedirects();
+		$this->logIn($this->login, $this->password);
+
 		$crawler = $this->client->request('GET', '/account/edit/details');
 		$this->assertTrue($this->client->getResponse()->isSuccessful());
 		$this->assertEquals('Ecommerce\Controller\AccountController::editDetailsAction', $this->client->getRequest()->attributes->get('_controller'));
@@ -102,7 +104,7 @@ class AccountControllerTest extends WebTestCase
 		$customer_details_repository = $this->em->getRepository('Ecommerce:CustomerDetails');
 
 		$customer_details = $customer_details_repository->findOneByCustomer($user);
-		fwrite(STDOUT, "\t" . 'old firstname:' . $customer_details->getFirstname(). PHP_EOL);
+		fwrite(STDOUT, "\t" . 'old firstname:' . $customer_details->getFirstname() . PHP_EOL);
 
 		//Edit details form
 		$form = $crawler->filter('form[action="/account/edit/details"]')->form();
@@ -116,7 +118,7 @@ class AccountControllerTest extends WebTestCase
 
 		//Check update
 		$customer_details = $customer_details_repository->findOneByCustomer($user);
-		fwrite(STDOUT, "\t" . 'new firstname:' . $customer_details->getFirstname(). PHP_EOL);
+		fwrite(STDOUT, "\t" . 'new firstname:' . $customer_details->getFirstname() . PHP_EOL);
 		$this->assertEquals('new Testfirst', $customer_details->getFirstname());
 
 		//Reset value
@@ -128,14 +130,132 @@ class AccountControllerTest extends WebTestCase
 		$this->client->submit($form);
 
 		$this->em->refresh($customer_details);
-		fwrite(STDOUT, "\t" . 'reset firstname:' . $customer_details->getFirstname(). PHP_EOL);
+		fwrite(STDOUT, "\t" . 'reset firstname:' . $customer_details->getFirstname() . PHP_EOL);
 		$this->assertEquals('Testfirst', $customer_details->getFirstname());
+	}
+
+	public function testEditEmail()
+	{
+		fwrite(STDOUT, PHP_EOL . __METHOD__ . PHP_EOL);
+
+		$this->logIn($this->login, $this->password);
+
+		$crawler = $this->client->request('GET', '/account/edit/email');
+		$this->assertTrue($this->client->getResponse()->isSuccessful());
+		$this->assertEquals('Ecommerce\Controller\AccountController::editEmailAction', $this->client->getRequest()->attributes->get('_controller'));
+
+		$user = $this->client->getContainer()->get('security.context')->getToken()->getUser();
+		fwrite(STDOUT, "\t" . 'old email:' . $user->getEmail() . PHP_EOL);
+
+		$new_email = 'test+1@email.com';
+
+		//Edit email form
+		$form = $crawler->filter('form[action="/account/edit/email"]')->form();
+		$form['form[email]'] = $new_email;
+		$form['form[password][first]'] = $this->password;
+		$form['form[password][second]'] = $this->password;
+		$this->client->submit($form);
+
+		//logout
+		$this->logOut();
+		$this->logIn($new_email, $this->password);
+
+		//Check update
+		$user = $this->client->getContainer()->get('security.context')->getToken()->getUser();
+		fwrite(STDOUT, "\t" . 'new email:' . $user->getEmail() . PHP_EOL);
+		$this->assertEquals($new_email, $user->getEmail());
+
+		//Reset value
+		$crawler = $this->client->request('GET', '/account/edit/email');
+		$form = $crawler->filter('form[action="/account/edit/email"]')->form();
+		$form['form[email]'] = $this->login;
+		$form['form[password][first]'] = $this->password;
+		$form['form[password][second]'] = $this->password;
+		$this->client->submit($form);
+
+		$this->logOut();
+		$this->logIn($this->login, $this->password);
+
+		$user = $this->client->getContainer()->get('security.context')->getToken()->getUser();
+		fwrite(STDOUT, "\t" . 'reset email:' . $user->getEmail() . PHP_EOL);
+		$this->assertEquals($this->login, $user->getEmail());
+	}
+
+	public function testEditPassword()
+	{
+		fwrite(STDOUT, PHP_EOL . __METHOD__ . PHP_EOL);
+
+		$this->logIn($this->login, $this->password);
+		$encoder = $this->container->get('security.password_encoder');
+
+		$crawler = $this->client->request('GET', '/account/edit/password');
+		$this->assertTrue($this->client->getResponse()->isSuccessful());
+		$this->assertEquals('Ecommerce\Controller\AccountController::editPasswordAction', $this->client->getRequest()->attributes->get('_controller'));
+
+		$user = $this->client->getContainer()->get('security.context')->getToken()->getUser();
+		fwrite(STDOUT, "\t" . 'old password:' . $user->getPassword() . PHP_EOL);
+		$this->assertTrue($encoder->isPasswordValid($user, $this->password));
+
+		$new_password = 'newte$tpassword';
+
+		//Edit email form
+		$form = $crawler->filter('form[action="/account/edit/password"]')->form();
+		$form['form[new_password]'] = $new_password;
+		$form['form[password][first]'] = $this->password;
+		$form['form[password][second]'] = $this->password;
+		$this->client->submit($form);
+
+		//logout
+		$this->logOut();
+		$this->logIn($this->login, $new_password);
+
+		//Check update
+		$user = $this->client->getContainer()->get('security.context')->getToken()->getUser();
+		fwrite(STDOUT, "\t" . 'new password:' . $user->getPassword() . PHP_EOL);
+		$this->assertTrue($encoder->isPasswordValid($user, $new_password));
+
+		//Reset value
+		$crawler = $this->client->request('GET', '/account/edit/password');
+		$form = $crawler->filter('form[action="/account/edit/password"]')->form();
+		$form['form[new_password]'] = $this->password;
+		$form['form[password][first]'] = $new_password;
+		$form['form[password][second]'] = $new_password;
+		$this->client->submit($form);
+
+		$this->logOut();
+		$this->logIn($this->login, $this->password);
+
+		$user = $this->client->getContainer()->get('security.context')->getToken()->getUser();
+		fwrite(STDOUT, "\t" . 'reset password:' . $user->getPassword() . PHP_EOL);
+		$this->assertTrue($encoder->isPasswordValid($user, $this->password));
 	}
 
 	public function tearDown()
 	{
 		$this->client->getKernel()->getContainer()->get('doctrine')->getConnection()->close();
 		parent::tearDown();
+	}
+
+	private function logIn($login, $password)
+	{
+		$session = $this->client->getContainer()->get('session');
+
+		$firewall = 'default';
+		$token = new UsernamePasswordToken($login, $password, $firewall, array());
+		$session->set('_security_' . $firewall, serialize($token));
+		$session->save();
+
+		$cookie = new Cookie($session->getName(), $session->getId());
+		$this->client->getCookieJar()->set($cookie);
+	}
+
+	private function logOut()
+	{
+		$session = $this->client->getContainer()->get('session');
+		$session->invalidate();
+		$session->clear();
+
+		$this->client->restart();
 	}
 
 }
